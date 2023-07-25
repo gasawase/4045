633794ffb31a593f5984142acaf780bff5a95ee7
Summer <49291773+gasawase@users.noreply.github.com>
2023-07-25 15:41:47 -0400

    project commit

---
+++ individualAssignment/individualAssignment/src/test/java/com/individualAssignment/individualAssignment/IndividualAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.individualAssignment.individualAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
